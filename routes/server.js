0832be4056bdc32568298aef27f4ee07e662e15e
Ansh Singh <81492907+ansh1101@users.noreply.github.com>
2021-10-13 12:02:29 +0530

    Update server.js

--- routes/server.js
+++ routes/server.js
@@ -3,21 +3,21 @@ const fs = require('fs');
 
 fs.copyFileSync('file1.txt' , 'server2.txt' );
 
-// const express = require('express');
+const express = require('express');
 
-// const app = express();
+const app = express();
 
-// app.get("/" , function(req, res){
-// res.send(`<h1>Calculator</h1> <input class="a"></input> <input class = "b"></input> 
-// <button class = "plus">+</button> <button>-</button> <button>*</button> <button>/</button> `);
-// });
+app.get("/" , function(req, res){
+res.send(`<h1>Calculator</h1> <input class="a"></input> <input class = "b"></input> 
+<button class = "plus">+</button> <button>-</button> <button>*</button> <button>/</button> `);
+});
 
-// app.listen(4000 , function(){
-//    console.log("Server Started");
-// });
-// var button = document.querySelector(".plus");
-// function add(){
-//     button.addEventListener("click", function(){
-//         document.querySelector(".a").innerHTML  +    document.querySelector(".b").innerHTML ;
-//     });
-// }
+app.listen(4000 , function(){
+   console.log("Server Started");
+});
+var button = document.querySelector(".plus");
+function add(){
+    button.addEventListener("click", function(){
+        document.querySelector(".a").innerHTML  +    document.querySelector(".b").innerHTML ;
+    });
+}
